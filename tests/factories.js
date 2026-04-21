2825a8c296dda67e0fa546d151856c431eebc6a3
Skhelshiaty <seif_elshiaty@yahoo.com>
2026-04-21 15:14:18 +0300

    Update factories.js

--- tests/factories.js
+++ tests/factories.js
@@ -2,7 +2,19 @@ const { faker } = require('@faker-js/faker');
 const { Product, Category } = require('../src/models/product');
 
 class ProductFactory {
-  
+  static data() {
+    return {
+      name: faker.commerce.productName(),
+      description: faker.commerce.productDescription(),
+      price: parseFloat(faker.commerce.price()),
+      available: faker.datatype.boolean(),
+      category: faker.helpers.arrayElement(Object.values(Category)),
+    };
+  }
+
+  static create() {
+    return ProductFactory.data();
+  }
 }
 
 module.exports = { ProductFactory };
